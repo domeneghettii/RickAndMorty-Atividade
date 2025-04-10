578acd57f4232c368f9f32c36a01e2ab1f8d1fb2
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-04-10 11:11:28 -0300

    first commit

---
+++ app/home/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import CharacterCard from "../../components/CharacterCard";
+import styles from "./home.module.css";
+
+
+export default function Home(){
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then((response) => {
+            setCharacters(response.data.results);
+        })
+        .catch((error) => {
+            console.error('Erro ao buscar personagens:', error);
+        });    
+}, []);
+
+console.log(characters);
+
+    return(
+        <div className={styles.container}>
+            <div className={styles.grid}>
+            {characters.map((char) => (
+            <CharacterCard key={char.id} character={char} />
+           ))}
+            </div>
+           
+        </div>
+       );
+}
